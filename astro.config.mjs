94f68dc56aed43105afdc8d555106c90200087c0
hamedrezaei-svg <rabinavin@gmail.com>
2026-05-31 22:33:10 -0700

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,3 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+export default defineConfig({ base: "/facepainting-website" });
